e7e2474474f990551c4effa25b26e6a7d0275b63
DenkyOgbonnaya <denkyogb@gmail.com>
2019-08-19 16:16:53 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+app.use(express.json());
+app.options('*', cors());
+app.use(cors({credentials:true, origin: 'http://localhost:3000'}));
+
+app.use( (err, req, res, next) => {
+    return res.status(400).send({status: 'error', message: err.message})
+})
+app.get('/', (req, res) => {
+    res.send('plan before you kkkllld');
+})
+if(process.env.NODE_ENV == 'production'){
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+    })
+}
+
+
+//listening port
+app.listen(port, (err) =>{
+    if(err) throw err ;
+        console.log(`BucketList listening on port ${port}`);
+} );
