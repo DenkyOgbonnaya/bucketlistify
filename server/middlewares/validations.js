cc1bf05533df1d77630cb9c2b6a69fd6e47df49d
DenkyOgbonnaya <denkyogb@gmail.com>
2019-08-20 12:25:58 +0100

    Add validations

---
+++ server/middlewares/validations.js
@@ -0,0 +1,25 @@
+const { check, validationResult } = require('express-validator/check');
+
+module.exports.validateBucketlist = [
+    check('name', 'BucketList name is required or too short').not().isEmpty().isLength({min: 2}),
+]
+module.exports.validateBucketitem = [
+    check('name', 'Item name is required or too short').not().isEmpty().isLength({min: 2}),
+    check('done', 'done must be true or false').optional().isIn(['true', 'false'])
+]
+module.exports.validateUserDetails= validateUserData = [
+    check('username', 'username is required').isLength({min: 2}),
+    check('email', 'valid email  is required').isEmail(),
+    check('password', 'password is required or too short').isLength({min: 3})
+]
+//validate login data
+module.exports.validateLoginDetails = [
+    check('username', 'username is required').isLength({min: 3}),
+    check('password', 'password is required').isLength({min: 3})
+]
+module.exports.checkValidationResult = (req, res, next) => {
+    const result = validationResult(req);
+    if(!result.isEmpty())
+        return res.status(400).send({message: result.array()[0].msg})
+    next();
+}
--- server/routes/bucketRoutes.js
+++ server/routes/bucketRoutes.js
@@ -1,6 +1,7 @@
 const BucketRouter = require('express').Router();
 const bucketController = require('../controllers/bucketController');
 const itemController = require('../controllers/itemController');
+const{validateBucketitem, validateBucketlist, checkValidationResult} = require('../middlewares/validations');
 
 const{
     createBucket, 
@@ -18,21 +19,21 @@ const{
 } = itemController;
 
 BucketRouter.route('/bucketlists')
-.post(createBucket)
+.post(validateBucketlist, checkValidationResult, createBucket)
 .get(getBucketList)
 
 BucketRouter.route('/bucketlists/:id')
 .get(getSingleBucket)
-.put(updateBucket)
+.put(validateBucketlist, checkValidationResult, updateBucket)
 .delete(deleteBucket)
 
 BucketRouter.route('/bucketlists/:id/items')
-.post(createItem)
+.post(validateBucketitem, checkValidationResult, createItem)
 .get(getItems)
 
 BucketRouter.route('/bucketlists/:id/items/:item_id')
 .get(getSingleItem)
-.put(updateItem)
+.put(validateBucketitem, checkValidationResult, updateItem)
 .delete(deleteItem)
 
 module.exports = BucketRouter
