f2f0773859a075ed0b756f431a4f70179f53670c
DenkyOgbonnaya <denkyogb@gmail.com>
2019-08-26 04:13:29 +0100

    Fix jwt malformed bug

--- client/src/components/bucketlist/api.js
+++ client/src/components/bucketlist/api.js
@@ -5,45 +5,63 @@ import axios from 'axios';
     axios.defaults.validateStatus = status => status < 500;
     
    export const createBucketlists = async (bucketlist) => {
+    const confiq = {
+        headers: {'Authorization': `Bearer ${localStorage.bearerToken}`}
+    }
        try{
-            return  await axios.post('/api/v1/bucketlists', bucketlist);
+            return  await axios.post('/api/v1/bucketlists', bucketlist, confiq);
        }catch(err){
            console.log(err); 
        }
    }
    export const getBucketlists = async (created_by, page, limit) => {
+    const confiq = {
+        headers: {'Authorization': `Bearer ${localStorage.bearerToken}`}
+    }
        try{
-           return await axios.get(`/api/v1/bucketlists?created_by=${created_by}&page=${page}&limit=${limit}`);
+           return await axios.get(`/api/v1/bucketlists?created_by=${created_by}&page=${page}&limit=${limit}`, confiq);
        }catch(err){
            console.log(err);
            
        }
    }
    export const searchBucketlists = async (created_by, search) => {
+    const confiq = {
+        headers: {'Authorization': `Bearer ${localStorage.bearerToken}`}
+    }
     try{
-        return await axios.get(`/api/v1/bucketlists?created_by=${created_by}&q=${search}`);
+        return await axios.get(`/api/v1/bucketlists?created_by=${created_by}&q=${search}`, confiq);
     }catch(err){
         console.log(err);
         
     }
 }
    export const getSingleBucketlists = async (id) => {
+    const confiq = {
+        headers: {'Authorization': `Bearer ${localStorage.bearerToken}`}
+    }
         try{
-            return await axios.get(`/api/v1/bucketlists/${id}`);
+            return await axios.get(`/api/v1/bucketlists/${id}`, confiq);
         }catch(err){
             console.log(err); 
         }
     }
     export const updateBucketlists = async (id, data) => {
+        const confiq = {
+            headers: {'Authorization': `Bearer ${localStorage.bearerToken}`}
+        }
         try{
-            return await axios.put(`/api/v1/bucketlists/${id}`, data);
+            return await axios.put(`/api/v1/bucketlists/${id}`, data, confiq);
         }catch(err){
             console.log(err); 
         }
     }
     export const deleteBucketlists = async (id) => {
+        const confiq = {
+            headers: {'Authorization': `Bearer ${localStorage.bearerToken}`}
+        }
         try{
-            return await axios.delete(`/api/v1/bucketlists/${id}`);
+            return await axios.delete(`/api/v1/bucketlists/${id}`, confiq);
         }catch(err){
             console.log(err); 
         }
--- client/src/components/items/api.js
+++ client/src/components/items/api.js
@@ -5,48 +5,66 @@ import axios from 'axios';
     axios.defaults.validateStatus = status => status < 500;
 
     export const createItem = async (id, data) => {
+        const confiq = {
+            headers: {'Authorization': `Bearer ${localStorage.bearerToken}`}
+        }
         try{
-            return await axios.post(`/api/v1//bucketlists/${id}/items`, data)
+            return await axios.post(`/api/v1//bucketlists/${id}/items`, data, confiq)
         }catch(err){
             console.log(err);
             
         }
     }
     export const getItems = async (id, page, limit) => {
+        const confiq = {
+            headers: {'Authorization': `Bearer ${localStorage.bearerToken}`}
+        }
         try{
-            return await axios.get(`/api/v1//bucketlists/${id}/items?page=${page}&limit=${limit}`)
+            return await axios.get(`/api/v1//bucketlists/${id}/items?page=${page}&limit=${limit}`, confiq)
         }catch(err){
             console.log(err);
             
         }
     }
     export const searchItems = async (id, search) => {
+        const confiq = {
+            headers: {'Authorization': `Bearer ${localStorage.bearerToken}`}
+        }
         try{
-            return await axios.get(`/api/v1//bucketlists/${id}/items?q=${search}`)
+            return await axios.get(`/api/v1//bucketlists/${id}/items?q=${search}`, confiq)
         }catch(err){
             console.log(err);
             
         }
     }
     export const getSingleItem = async (bucketlistsId, itemId) => {
+        const confiq = {
+            headers: {'Authorization': `Bearer ${localStorage.bearerToken}`}
+        }
         try{
-            return await axios.get(`/api/v1//bucketlists/${bucketlistsId}/items/${itemId}`)
+            return await axios.get(`/api/v1//bucketlists/${bucketlistsId}/items/${itemId}`, confiq)
         }catch(err){
             console.log(err);
             
         }
     }
     export const updateItem = async (bucketlistsId, itemId, data) => {
+        const confiq = {
+            headers: {'Authorization': `Bearer ${localStorage.bearerToken}`}
+        }
         try{
-            return await axios.put(`/api/v1//bucketlists/${bucketlistsId}/items/${itemId}`, data)
+            return await axios.put(`/api/v1//bucketlists/${bucketlistsId}/items/${itemId}`, data, confiq)
         }catch(err){
             console.log(err);
             
         }
     }
     export const deleteItem = async (bucketlistsId, itemId) => {
+        const confiq = {
+            headers: {'Authorization': `Bearer ${localStorage.bearerToken}`}
+        }
         try{
-            return await axios.delete(`/api/v1//bucketlists/${bucketlistsId}/items/${itemId}`)
+            return await axios.delete(`/api/v1//bucketlists/${bucketlistsId}/items/${itemId}`, confiq)
         }catch(err){
             console.log(err);
             
